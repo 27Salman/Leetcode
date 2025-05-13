14f64a23c40fab4dd1bf6325a2250f51a9fb889b
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-05-13 11:35:48 +0530

    Add files via upload

---
+++ Leetcode_896.js
@@ -0,0 +1,15 @@
+//Monotonic Array
+var isMonotonic = function(nums) {
+    let increasing = true;
+    let decreasing = true;
+
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>nums[i+1]){
+            increasing = false;
+        }
+        if(nums[i]<nums[i+1]){
+            decreasing = false;
+        }
+    }
+    return increasing||decreasing;
+};
