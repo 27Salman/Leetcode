e43ef95b06f3302bd2edd3eb735ce5b8c14f3a42
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-06-15 07:24:04 +0530

    Add files via upload

---
+++ Leetcode_2089.js
@@ -0,0 +1,10 @@
+//Find Target Indices After Sorting Array
+var targetIndices = function(nums, target) {
+    nums.sort((a,b)=>a-b);
+    let arr=[];
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==target){
+            arr.push(i);
+        }
+    }return arr;
+};
