8c5689835e3be0328fbb64c7ea5da2d7580aadd0
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-07-24 10:33:22 +0530

    Add files via upload

---
+++ Leetcode_3110.js
@@ -0,0 +1,6 @@
+var scoreOfString = function(s) {
+    let result = 0;
+    for(let i = 1; i < s.length; i++){
+        result += Math.abs(s.charCodeAt(i-1) - s.charCodeAt(i));
+    }return result;
+};
