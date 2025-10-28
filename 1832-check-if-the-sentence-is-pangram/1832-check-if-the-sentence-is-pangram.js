36c3e3de8087b2f73ea3ed1c8cc1aca244619ac6
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-10-28 16:56:57 +0530

    Time: 5 ms (5.88%), Space: 54.8 MB (66.5%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -3,12 +3,15 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    let arr=[/a-z/];
+    let arr=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     let count = 0;
+    let arr1=[];
     for(let i = 0; i<=sentence.length;i++){
-        if(arr.includes(sentence[i])){
+        if(arr.includes(sentence[i]) && !arr1.includes(sentence[i])){
+            arr1.push(sentence[i]);
             count++;
         }
     }
-    return count===sentence.length;
+    console.log(count);
+    return count===arr.length;
 };
