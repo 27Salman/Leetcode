1ccbc5e9f4c782b3f5b3b65e2e0568f28f909c44
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-11-05 07:06:49 +0530

    Time: 32 ms (8.65%), Space: 57.7 MB (60.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,11 +4,5 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    s = s.split("").sort().join("");
-    t = t.split("").sort().join("");
-    if(s===t){
-        return true;
-    }else{
-        return false;
-    }
+    return s.split("").sort().join("") === t.split("").sort().join("");
 };
