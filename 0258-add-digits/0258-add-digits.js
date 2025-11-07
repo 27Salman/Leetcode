738883b7d7b2b6999e11012bc9fd62929a21bc6c
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-11-07 19:31:37 +0530

    Time: 1 ms (53.43%), Space: 57.8 MB (14.36%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,12 +3,9 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    function rma(c){
-        return c.reduce((a,b)=>Number(a) + Number(b),0);
-    } 
     while(num >= 1 ){
         let str = num.toString().split("");
-        let n1 = rma(str);
+        let n1 = str.reduce((a,b)=>Number(a) + Number(b),0);
         num = n1;
         if(num<10 && num > 0) break;
     }return num;
