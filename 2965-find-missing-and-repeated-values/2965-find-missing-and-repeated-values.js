17bad1a65dc43d1b6a95ce8af71d9f6b6654fede
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-11-10 18:53:22 +0530

    Time: 92 ms (10.56%), Space: 56.3 MB (88.73%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.js
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.js
@@ -5,13 +5,15 @@
 var findMissingAndRepeatedValues = function(grid) {
     let arr = []
     let num = grid.flat(Infinity);
-    let num1 = num.reduce((acc,cur)=>{
-        acc[cur] = (acc[cur] || 0)+1;
-        return acc;
-    },{});
-    for(let n in num1){
-        if(num1[n]>1) arr.push(Number(n));
-    }
+    let num1 = num.filter((value,index)=> num.indexOf(value)!==index);
+    arr.push(Number(num1));
+    // let num1 = num.reduce((acc,cur)=>{
+    //     acc[cur] = (acc[cur] || 0)+1;
+    //     return acc;
+    // },{});
+    // for(let n in num1){
+    //     if(num1[n]>1) arr.push(Number(n));
+    // }
     for(let i = 1; i<=num.length;i++){
         if(!num.includes(i)) arr.push(i);
     }
