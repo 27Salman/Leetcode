88c1219219222982855b663637f03772a9290669
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-11-13 08:16:42 +0530

    Time: 3 ms (2.79%), Space: 54.5 MB (19.12%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var sumOfTheDigitsOfHarshadNumber = function(x) {
+    let n = x.toString().split("");
+    let sum = n.reduce((a,b)=>Number(a)+Number(b),0);
+    console.log(sum)
+    if(x%sum === 0) return sum;
+    return -1;
+};
