a9b0d7cbb2bedce006723ab164ecaeb5e063ecdf
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-11-13 08:18:33 +0530

    Time: 0 ms (100%), Space: 53 MB (94.42%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.js
+++ 3099-harshad-number/3099-harshad-number.js
@@ -3,8 +3,7 @@
  * @return {number}
  */
 var sumOfTheDigitsOfHarshadNumber = function(x) {
-    let n = x.toString().split("");
-    let sum = n.reduce((a,b)=>Number(a)+Number(b),0);
-    if(x%sum === 0) return sum;
+    let n = x.toString().split("").reduce((a,b)=>Number(a)+Number(b),0);
+    if(x%n === 0) return n;
     return -1;
 };
