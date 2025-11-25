7e3028f2006b555f9cb84a9548c48e68173434e1
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-11-25 09:34:37 +0530

    Time: 60 ms (30.07%), Space: 55.4 MB (79.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var reverse = function(x) {
-    let n = Math.abs(x).toString().split('').reverse().join('');
-    let result = n*Math.sign(x);
+    let result = Math.abs(x).toString().split('').reverse().join('') * Math.sign(x);
     return ( result<-(2**31) || result>2**31 ) ? 0 : result;
 };
