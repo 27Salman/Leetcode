6f408c5aab1c48ec2a8ce1252dc14befc4a3052a
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-01 17:04:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,26 +4,35 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    //let i = 0;
-    let arr = [];
-    let n = nums.length;
-    if(!nums.includes(target) || nums.length === 0) return [-1,-1];
-    for(let i = 0; i<=n; i++){
-        if(nums[i]===target){
-            arr.push(i);
-            break;
-        }
-    }
-    for(let j = n-1; j>=0; j--){
-        if(nums[j]===target){
-            arr.push(j);
-            break;
-        }
-    }return arr;
-    // while(i<=nums.length){
+    // let arr = [];
+    // let n = nums.length;
+    // if(!nums.includes(target) || nums.length === 0) return [-1,-1];
+    // for(let i = 0; i<=n; i++){
     //     if(nums[i]===target){
     //         arr.push(i);
+    //         break;
     //     }
-    //     i++;
+    // }
+    // for(let j = n-1; j>=0; j--){
+    //     if(nums[j]===target){
+    //         arr.push(j);
+    //         break;
+    //     }
     // }return arr;
+
+    let i = 0;
+    let n = nums.length;
+    let left = 0;
+    let right = n-1;
+    if(!nums.includes(target) || nums.length === 0) return [-1,-1];
+    while(left<right){
+        if(nums[left]===target){
+            arr.push(left);
+            left++;
+        }
+        if(nums[right]===target){
+            arr.push(right);
+            rigth--;
+        }
+    }return arr;
 };
