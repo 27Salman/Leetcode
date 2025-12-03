ab7b4305e70b78e71ade5c3553a170dec5495607
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-03 14:05:26 +0530

    Time: 61 ms (22.5%), Space: 57.9 MB (32.73%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -6,7 +6,8 @@
 var shuffle = function(nums, n) {
     let arr = [];
     for(let i = 0; i<=n/2; i++){
-        arr.push(nums[i],nums[n]);
+        arr.push(nums[i]);
+        arr.push(nums[n]);
         if(nums.length-1!==n) n++;
     }return arr;
 };
