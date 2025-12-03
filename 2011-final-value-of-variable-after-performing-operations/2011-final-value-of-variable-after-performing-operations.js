4b6f56a75a89890492317bf5da46cd2839df7372
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-03 11:23:22 +0530

    Time: 0 ms (100%), Space: 56.5 MB (16.7%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -3,9 +3,8 @@
  * @return {number}
  */
 var finalValueAfterOperations = function(operations) {
-    const count = operations.reduce((acc,cur)=>{
-        (cur === "++X" || cur === "X++") ? acc++ : acc--; 
-        return acc;
-    },0);
-    return count;
+    return operations.reduce((acc,cur)=>{
+            (cur === "++X" || cur === "X++") ? acc++ : acc--; 
+            return acc;
+            },0);
 };
