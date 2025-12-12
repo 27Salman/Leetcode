ce5d451e56d913ed397b38b252daea8649769b4e
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-12 14:55:40 +0530

    Time: 1 ms (85.98%), Space: 55.8 MB (81.22%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.js
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.js
@@ -3,12 +3,12 @@
  * @return {number[]}
  */
 var getSneakyNumbers = function(nums) {
-    let arr = [];
-    let num = nums.reduce((acc,cur)=>{
-        acc[cur] = (acc[cur]||0) +1;
-        return acc;
-    },{});
-    for(let nums in num){
-        if(num[nums]>1) arr.push(Number(nums));
-    }return arr;
+    return nums.filter((val,index)=>nums.indexOf(val)!==index);
+    // let num = nums.reduce((acc,cur)=>{
+    //     acc[cur] = (acc[cur]||0) +1;
+    //     return acc;
+    // },{});
+    // for(let nums in num){
+    //     if(num[nums]>1) arr.push(Number(nums));
+    // }return arr;
 };
