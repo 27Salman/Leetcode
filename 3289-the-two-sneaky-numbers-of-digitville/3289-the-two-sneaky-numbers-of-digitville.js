70967b876d7d4075a0b985df8ba27d1b5fc7faee
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-12 14:56:45 +0530

    Time: 1 ms (85.98%), Space: 55.7 MB (87.15%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.js
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.js
@@ -4,11 +4,4 @@
  */
 var getSneakyNumbers = function(nums) {
     return nums.filter((val,index)=>nums.indexOf(val)!==index);
-    // let num = nums.reduce((acc,cur)=>{
-    //     acc[cur] = (acc[cur]||0) +1;
-    //     return acc;
-    // },{});
-    // for(let nums in num){
-    //     if(num[nums]>1) arr.push(Number(nums));
-    // }return arr;
 };
