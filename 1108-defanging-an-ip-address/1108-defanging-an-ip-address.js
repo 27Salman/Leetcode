7578ea2a9eb9e7f4fa956c484959bfdc6cb1456a
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-14 08:27:45 +0530

    Time: 47 ms (46.55%), Space: 54.5 MB (11.27%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -3,5 +3,11 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    return address.split(".").join("[.]").toString();
+   // return address.split(".").join("[.]").toString();
+   let num = address.split("")
+   for(let i = 0 ; i<num.length; i++){
+    if(num[i]==="."){
+        num[i]="[.]"
+    }
+   }return num.join("");
 };
