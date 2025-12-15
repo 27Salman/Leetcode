6a537e0147076e66d5857eb4a761f7b509db831b
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-15 08:19:00 +0530

    Time: 2 ms (69.46%), Space: 57.6 MB (50%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.js
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.js
@@ -4,10 +4,6 @@
  */
 var transformArray = function(nums) {
     for(let i = 0; i<nums.length; i++){
-        if(nums[i]%2===0){
-            nums[i]=0;
-        }else{
-            nums[i]=1;
-        }
+        nums[i]%2===0 ? nums[i]=0 : nums[i]=1;
     }return nums.sort((a,b)=>a-b);
 };
