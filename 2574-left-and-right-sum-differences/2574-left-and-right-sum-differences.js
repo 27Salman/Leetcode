66b3202cc4f6a77e7f4e9a16026c6f2549e4cf3c
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-16 11:02:20 +0530

    Time: 2 ms (75.25%), Space: 58.5 MB (42.24%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -10,11 +10,10 @@ var leftRightDifference = function(nums) {
 
     for(let i = 1; i<n;i++){
         leftSum[i] = leftSum[i-1]+nums[i-1];
-        rightSum[n-i-1] = rightSum[n-i] + nums[n-i];
     }
-    // for (let i = n - 2; i >= 0; i--) {
-    //     rightSum[i] = rightSum[i + 1] + nums[i + 1];
-    // }
+    for (let i = n - 2; i >= 0; i--) {
+        rightSum[i] = rightSum[i + 1] + nums[i + 1];
+    }
     for(let i = 0; i<n; i++){
         result[i] = Math.abs(leftSum[i]-rightSum[i]);
     }return result;
