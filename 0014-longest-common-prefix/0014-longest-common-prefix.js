df527b206884beade2d0d704a8c69691a60a06f7
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-19 10:42:16 +0530

    Time: 0 ms (100%), Space: 54.7 MB (60.24%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -4,13 +4,12 @@
  */
 var longestCommonPrefix = function(strs) {
     if(strs.length === 0) return "";
-    let prefix = strs[0];
-    for(let i = 1; i<strs.length; i++){
-        let j = 0;
-        while(j < prefix.length && j < strs[i].length && prefix[j]===strs[i][j]){
-            j++;
+    for(let i = 0; i<strs[0].length; i++){
+        let char = strs[0][i];
+        for(let j = 1; j<strs.length; j++){
+            if(i >= strs[j].length || strs[j][i] !== char){
+                return strs[0].slice(0,i);
+            }
         }
-        prefix = prefix.slice(0,j);
-        if(prefix === "") return "";
-    }return prefix;
+    }return strs[0];
 };
