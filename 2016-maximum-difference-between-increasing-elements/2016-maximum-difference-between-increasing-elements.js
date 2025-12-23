94dfabdad7766d90e2d502f8d7f2e6aa33dc4731
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-23 21:41:18 +0530

    Time: 4 ms (44.36%), Space: 53.8 MB (88%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.js
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var maximumDifference = function(nums) {
-    let max = 0;
+    let max = -1;
     for(let i = 0; i<nums.length; i++){
         for(let j = i+1; j<nums.length; j++){
             if(nums[i] < nums[j]){
                 max = Math.max(max,nums[j]-nums[i]);
             }
         }
-    }return max !== 0 ? max:-1;
+    }return max;
 };
