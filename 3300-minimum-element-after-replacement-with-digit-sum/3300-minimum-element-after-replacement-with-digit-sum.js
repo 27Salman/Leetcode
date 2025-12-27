36ed7e88d3d1e6355ef737c6fc9347ed6d9f87a0
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-12-27 09:27:44 +0530

    Time: 3 ms (49.52%), Space: 56.9 MB (49.52%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.js
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.js
@@ -5,6 +5,6 @@
 var minElement = function(nums) {
     let arr = [];
     for(let i = 0; i<nums.length; i++){
-        arr[i]=nums[i].toString().split("").reduce((a,b)=>Number(a)+Number(b),0);
-    }return Math.min(...arr);
+        nums[i]=nums[i].toString().split("").reduce((a,b)=>Number(a)+Number(b),0);
+    }return Math.min(...nums);
 };
