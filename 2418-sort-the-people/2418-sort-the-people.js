ce6e7a885db466ed278be30c8472e64da0d856e8
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-01-04 13:22:26 +0530

    Time: 7 ms (58.76%), Space: 60 MB (41.58%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.js
+++ 2418-sort-the-people/2418-sort-the-people.js
@@ -4,19 +4,7 @@
  * @return {string[]}
  */
 var sortPeople = function(names, heights) {
-    for(let i = 0; i<heights.length; i++){
-        for(let j = i+1; j<heights.length; j++){
-            if(heights[i]<heights[j]){
-                let temp = 0;
-                temp = heights[i];
-                heights[i]=heights[j];
-                heights[j]=temp;
-
-                let str = "";
-                str = names[i];
-                names[i]=names[j];
-                names[j]=str;
-            }
-        }
-    }return [...names];
+    return names.map( (names,i) => ( {names, heights:heights[i]} ) )
+                .sort( (a,b) => b.heights-a.heights )
+                .map( person => person.names )
 };
