06427da0f12f60c52154e3934ab12d8027f7b00a
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-01-09 06:49:35 +0530

    Time: 0 ms (100%), Space: 53.3 MB (97.6%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var pivotInteger = function(n) {
+    let total = (n*(n+1))/2;
+    let x = Math.sqrt(total);
+    return Number.isInteger(x) ? x : -1;
+};
