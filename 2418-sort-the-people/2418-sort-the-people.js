d226566ac70e65a7972879414266db2ec8436c8d
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-01-10 17:44:18 +0530

    Time: 7 ms (58.76%), Space: 60.6 MB (33.68%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.js
+++ 2418-sort-the-people/2418-sort-the-people.js
@@ -6,5 +6,5 @@
 var sortPeople = function(names, heights) {
     return names.map( (names,i) => ( {names, heights:heights[i]} ) )
                 .sort( (a,b) => b.heights-a.heights )
-                .map( person => person.names )
+                .map( person => person.names );
 };
