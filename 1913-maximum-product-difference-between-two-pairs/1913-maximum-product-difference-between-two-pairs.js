090ee0fe2ed340a3dab0361b50f3cc9dda29919d
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-01-16 22:59:19 +0530

    Time: 39 ms (97.52%), Space: 55.8 MB (96.69%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
@@ -3,7 +3,25 @@
  * @return {number}
  */
 var maxProductDifference = function(nums){
-    let min = nums.sort((a,b)=>a-b);
-    let n = min.length;
-    return ((min[n-1]*min[n-2])-(min[0]*min[1]))
+    let max1 = 0, max2=0;
+    let min1 = Infinity, min2 = Infinity;
+
+    for (let i = 0; i<nums.length; i++){
+        let val = nums[i];
+
+        if(val>max1){
+            max2 = max1;
+            max1 = val;
+        }else if(val > max2){
+            max2 = val;
+        }
+
+        if(val<min1){
+            min2 = min1;
+            min1 = val;
+        }else if(val<min2){
+            min2 = val;
+        }
+    }
+    return (max1*max2) - (min1*min2);
 };
