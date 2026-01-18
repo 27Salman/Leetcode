4735d805a2ea46b626c5642032ce82a5de37d9ab
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-01-18 23:06:04 +0530

    Time: 0 ms (100%), Space: 56.3 MB (43.06%) - LeetHub

---
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var addedInteger = function(nums1, nums2) {
+    nums1.sort((a,b)=>a-b);
+    nums2.sort((a,b)=>a-b);
+    return (Math.min(...nums2) - Math.min(...nums1))
+};
