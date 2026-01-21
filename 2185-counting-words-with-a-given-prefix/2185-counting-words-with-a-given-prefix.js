c0825ccffd621ca649b65ae155ab64de4ac10a67
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-01-21 19:57:02 +0530

    Time: 3 ms (16.67%), Space: 54.2 MB (93.14%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+var prefixCount = function(words, pref) {
+    let count = 0;
+    for(let str of words){
+        let flag = false;
+        for(let i = 0; i<pref.length; i++){
+            if(str[i]===pref[i]){
+                flag = true;
+                continue;
+            }else{
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            count++;
+        }
+    }return count;
+};
