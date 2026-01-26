12b3de138d5ba26d6514be181e83db95e03db709
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-01-26 12:07:04 +0530

    Time: 39 ms (86.5%), Space: 54.3 MB (47.82%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let str = "";
+    let num = word1.length + word2.length;
+    for(let i = 0; i<num; i++){
+        if(i<word1.length) str += word1[i]
+        if(i<word2.length) str += word2[i]
+    }return str;
+};
