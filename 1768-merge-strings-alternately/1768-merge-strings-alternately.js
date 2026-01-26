537d8a6049d2f989b05ef1fee61440a8998f1ba2
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-01-26 12:17:41 +0530

    Time: 39 ms (86.5%), Space: 53.9 MB (70.82%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -5,7 +5,7 @@
  */
 var mergeAlternately = function(word1, word2) {
     let str = "";
-    let num = word1.length + word2.length;
+    let num = Math.max(word1.length, word2.length);
     for(let i = 0; i<num; i++){
         if(i<word1.length) str += word1[i]
         if(i<word2.length) str += word2[i]
