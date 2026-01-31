024ac7d3baa4a5ea136e5a0e757b032ff47b8fd0
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-01-31 22:31:39 +0530

    Time: 0 ms (100%), Space: 56.1 MB (86%) - LeetHub

---
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maximumNumberOfStringPairs = function(words) {
+    let count = 0;
+    for(let i = 0; i<words.length; i++){
+        let rev = words[i].split("").reverse().join("")
+        for(let j = i+1; j<words.length; j++){
+            if(rev===words[j]){
+                count++;
+            };
+        }
+    }return count;
+};
