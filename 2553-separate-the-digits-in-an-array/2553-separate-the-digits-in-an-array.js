4670f6dac2d76645590cb3b83a3283b50e79de3a
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-02-02 10:25:23 +0530

    Time: 3 ms (80.33%), Space: 56.3 MB (99.18%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var separateDigits = function(nums) {
-    let arr = [];
-    for(let num of nums){
-        let str = num.toString().split("")
-        for(let i = 0; i<str.length; i++){
-            arr.push(Number(str[i]))
-        }
-    }return arr;
+    return nums.join("").split("").map(Number);
 };
