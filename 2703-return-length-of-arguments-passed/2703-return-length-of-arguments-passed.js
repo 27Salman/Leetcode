27385c35f212d747631fcff10d2656599e92ef1f
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-02-07 07:51:21 +0530

    Time: 36 ms (88.45%), Space: 54.4 MB (13.6%) - LeetHub

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -3,11 +3,8 @@
  * @return {number}
  */
 var argumentsLength = function(...args) {
-    let count = 0;
-    for(let i=0;i<args.length;i++){
-        count++;
-    }
-    return count;
+    let  arr = [...args];
+    return arr.length;
 };
 
 /**
