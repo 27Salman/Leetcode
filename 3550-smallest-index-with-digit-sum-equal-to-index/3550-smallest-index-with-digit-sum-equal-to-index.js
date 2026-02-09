40697bc673d93bee4414496a71551c50450cb9f8
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-02-09 23:10:19 +0530

    Time: 3 ms (21.09%), Space: 56.3 MB (54.69%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.js
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.js
@@ -3,12 +3,9 @@
  * @return {number}
  */
 var smallestIndex = function(nums) {
-    let min = 0;
     for(let i = 0; i<nums.length; i++){
         let n = nums[i].toString().split("").reduce((acc,cur)=> acc+Number(cur), 0);
-        if(Number(n) === i){
-            return i;
-        }
+        if(n === i) return i;
     }
     return -1;
 };
