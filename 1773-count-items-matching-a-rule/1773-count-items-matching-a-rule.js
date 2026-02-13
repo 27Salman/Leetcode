1892ec4db1f165daae40d0a588816ddfde9d6b5a
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-02-13 08:18:28 +0530

    Time: 4 ms (9.15%), Space: 60.1 MB (28.05%) - LeetHub

---
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+    let count = 0;
+    for(let item of items){
+        if(ruleKey === "type"){
+            if(ruleValue === item[0]){
+                count++;
+            }
+        }else if(ruleKey === "color"){
+            if(ruleValue === item[1]){
+                count++;
+            }
+        }else if(ruleKey === "name"){
+            if(ruleValue === item[2]){
+                count++;
+            }
+        }
+    }return count;
+};
