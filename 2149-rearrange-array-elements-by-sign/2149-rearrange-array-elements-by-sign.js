4448f3ec1a40010abf2111c546b9fefd5f4eec78
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-02-16 20:54:20 +0530

    Time: 10 ms (93.59%), Space: 99.1 MB (38.45%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
@@ -3,26 +3,17 @@
  * @return {number[]}
  */
 var rearrangeArray = function(nums) {
-    let positive = [];
-    let negative = [];
-    let result = [];
     let n = nums.length;
+    let result = new Array(n);
+    let positive = 0;
+    let negative = 1;
     for(let i = 0; i<n; i++){
         if(nums[i]>=0){
-            positive.push(nums[i]);
+            result[positive] = nums[i];
+            positive += 2;
         }else{
-            negative.push(nums[i]);
-        }
-    }
-    let x = 0;
-    let y = 0;
-    for(let j = 0; j<n; j++){
-        if(j%2 === 0 ){
-            result.push(positive[x]);
-            x++;
-        }else{
-            result.push(negative[y]);
-            y++
+            result[negative] = nums[i];
+            negative += 2;
         }
     }return result;
 };
