016445c185c222667e45ba604395262eb9596a87
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-02-18 12:20:49 +0530

    Time: 2 ms (52.48%), Space: 56.4 MB (73.76%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.js
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.js
@@ -3,12 +3,9 @@
  * @return {string}
  */
 var removeTrailingZeros = function(num) {
-    let str ="";
-    for(let i = num.length-1; i>=0; i--){
-        if(num[i]==="0"){  
-            str = num.slice(0,i);
-        }else{
-            break;
-        }
-    }return str.length === 0 ? num:str
+    let i = num.length - 1;
+    while(i>=0 && num[i]==="0"){
+        i--;
+    }
+    return num.substring(0,i+1);
 };
