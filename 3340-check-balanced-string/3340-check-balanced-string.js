cc7341ade808f1a588de68b61a5b42a30602bef7
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-02-23 06:35:43 +0530

    Time: 1 ms (52.34%), Space: 57.3 MB (27.1%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var isBalanced = function(num) {
+    let evenSum = 0;
+    let oddSum = 0;
+    for(let i  = 0; i<num.length; i++){
+        if(i%2===0){
+            evenSum += Number(num[i]);
+        }else{
+            oddSum += Number(num[i]);
+        }
+    }
+    return evenSum === oddSum ? true:false;
+};
