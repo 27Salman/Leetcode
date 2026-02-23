a4bf3603ec6372094e1b89b2175e0833a6c1e31a
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-02-23 06:31:12 +0530

    Time: 0 ms (100%), Space: 55.5 MB (73.23%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var gcdOfOddEvenSums = function(n) {
+    let sumOdd = n * n;
+    let sumEven = n * (n + 1);
+    const getGCD = (a, b) => {
+        while (b !== 0) {
+            let temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    };
+    return getGCD(sumOdd, sumEven);
+};
