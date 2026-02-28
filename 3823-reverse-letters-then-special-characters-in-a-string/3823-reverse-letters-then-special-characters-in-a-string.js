04a9ea59a2794650dda37d348878a0f521cb8aef
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-02-28 20:21:04 +0530

    Time: 4 ms (60.9%), Space: 58.8 MB (23.79%) - LeetHub

--- 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.js
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.js
@@ -11,13 +11,11 @@ var reverseByType = function(s) {
         if (/[a-zA-Z]/.test(char)) letters.push(char);
         else if (/[^a-zA-Z0-9]/.test(char)) specials.push(char);
     }
-
-    // 2. Put them back in, but pull from the end of the arrays (reversing them)
     for (let i = 0; i < result.length; i++) {
         if (/[a-zA-Z]/.test(result[i])) {
-            result[i] = letters.pop(); // Reverses letters
+            result[i] = letters.pop(); 
         } else if (/[^a-zA-Z0-9]/.test(result[i])) {
-            result[i] = specials.pop(); // Reverses specials
+            result[i] = specials.pop(); 
         }
     }
 
