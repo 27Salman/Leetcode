41ec3bbb7c2bba26ad63f14cd012f76affcf07de
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-03-05 06:42:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
-    const counts = new Map();
+    const count = new Map();
     for (const num of arr) {
-        counts.set(num, (counts.get(num) || 0) + 1);
+        count.set(num, (count.get(num) || 0) + 1);
     }    
-    const frequencies = Array.from(counts.values());
-    const uniqueFrequencies = new Set(frequencies);
-    return frequencies.length === uniqueFrequencies.size;
+    const freq = Array.from(count.values());
+    const uniqFreq = new Set(freq);
+    return freq.length === uniqFreq.size;
 };
