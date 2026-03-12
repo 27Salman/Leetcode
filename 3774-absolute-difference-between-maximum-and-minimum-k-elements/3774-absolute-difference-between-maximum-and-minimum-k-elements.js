49e190417bfc4bc089524f031e6dcb5cf2e411b6
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-03-12 15:09:23 +0530

    Time: 3 ms (58.08%), Space: 56.7 MB (46.71%) - LeetHub

---
+++ 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var absDifference = function(nums, k) {
+    let num = nums.sort((a,b)=>b-a);
+    let sum1 = 0;
+    let sum2 = 0;
+    for(let i = 0; i<k; i++){
+        sum1 += nums[i];
+        sum2 += nums[nums.length-i-1];
+    }return Math.abs(sum1-sum2);
+};
