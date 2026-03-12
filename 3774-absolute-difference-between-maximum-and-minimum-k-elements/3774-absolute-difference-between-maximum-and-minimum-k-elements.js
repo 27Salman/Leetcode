62fd865a83f608890ca2987b24f1da890eb42b8a
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-03-12 15:11:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.js
+++ 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.js
@@ -8,7 +8,7 @@ var absDifference = function(nums, k) {
     let sum1 = 0;
     let sum2 = 0;
     for(let i = 0; i<k; i++){
-        sum1 += nums[i];
-        sum2 += nums[nums.length-i-1];
+        sum1 += num[i];
+        sum2 += num[nums.length-i-1];
     }return Math.abs(sum1-sum2);
 };
