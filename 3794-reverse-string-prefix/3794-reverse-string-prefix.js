0988230ba3c56a3c2cac6b086e97612649358411
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-03-15 14:45:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3794-reverse-string-prefix/3794-reverse-string-prefix.js
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.js
@@ -4,5 +4,5 @@
  * @return {string}
  */
 var reversePrefix = function(s, k) {
-    return k!==1 ? s.slice(0,k).split("").reverse().join("")+s.slice(k):s;
+    return k!==1 ? s.slice(0,k).split("").reverse().join("") + s.slice(k) : s;
 };
