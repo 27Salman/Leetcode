2f13615e69955e8d767a7964e67c0ab718cc93b1
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-03-25 21:42:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3736-minimum-moves-to-equal-array-elements-iii/3736-minimum-moves-to-equal-array-elements-iii.js
+++ 3736-minimum-moves-to-equal-array-elements-iii/3736-minimum-moves-to-equal-array-elements-iii.js
@@ -6,9 +6,6 @@ var minMoves = function(nums) {
     let max = Math.max(...nums);
     let count = 0;
     for(let i = 0; i<nums.length; i++){
-        while(nums[i]!==max){
-            nums[i] += 1;
-            count++;
-        }
+        count += max - nums[i];
     }return count;
 };
