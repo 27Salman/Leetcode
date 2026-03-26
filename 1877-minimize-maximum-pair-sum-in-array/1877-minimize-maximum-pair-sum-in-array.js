3ffefd744c8070f7c30a3bd69edac9c97584baeb
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-03-26 17:09:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.js
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.js
@@ -4,10 +4,10 @@
  */
 var minPairSum = function(nums) {
     let n = nums.sort((a,b)=>a-b);
-    let left = 0, right = nums.length-1, half = nums.length/2;
+    let left = 0, right = nums.length-1;
     let value = 0;
     while(left<=right){
-        let a = nums[left]+nums[right];
+        let a = n[left]+n[right];
         left++;
         right--;
         if(a>value) value = a;
