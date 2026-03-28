44cab15965897885970170723db797428c175ca8
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-03-28 10:45:26 +0530

    Time: 13 ms (28.8%), Space: 60.3 MB (40.8%) - LeetHub

---
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var hasSameDigits = function(s) {
+    while(3 <= s.length){
+         let str = "";
+        for(let i = 1; i<s.length; i++){
+            str += (Number(s[i])+Number(s[i-1]))%10;
+        }
+        s = str;
+    }return s[0]==s[1] ? true : false;
+};
