8336e50aeaa8a62ee747a9c7c9d5e779667c7910
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-06-10 21:53:50 +0530

    Time: 2 ms (71.82%), Space: 55.5 MB (25.77%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.js
+++ 0507-perfect-number/0507-perfect-number.js
@@ -3,9 +3,15 @@
  * @return {boolean}
  */
 var checkPerfectNumber = function(num) {
-    if(num===1) return false;
+    if(num <=1) return false;
     let sum = 1;
-    for(let i = 2; i<=num/2; i++){
-        if(num%i === 0) sum += i;
-    }return sum === num 
+
+    for(let i = 2; i*i<=num; i++){
+        if(num%i === 0) {
+            sum += i;
+            if(i!==num/i){
+                sum += num/i;
+            }
+        }
+    }return sum === num;
 };
