32b618295745c4d3684e9d2caf569838322ad003
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-06-17 19:57:06 +0530

    Time: 3 ms (80.4%), Space: 55.2 MB (83.92%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function(paragraph, banned) {
+    let words = paragraph.match(/[a-zA-Z]+/g).map(a => a.toLowerCase()).reduce((acc,cur)=>{
+        acc[cur] = (acc[cur]||0)+1;
+        return acc;
+    },{});
+    let result = 0;
+    let answer = ""
+    for(let word in words){
+        if(banned.includes(word)) continue;
+        if(words[word] >result){
+            result = words[word];
+            answer = word
+        };
+
+    };
+    return answer;
+};
