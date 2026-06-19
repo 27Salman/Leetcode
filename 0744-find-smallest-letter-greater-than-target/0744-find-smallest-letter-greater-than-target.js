08e08ede5a9c39c39d8b257d68c84a17c4cf7b19
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-06-20 00:08:48 +0530

    Time: 0 ms (100%), Space: 53.9 MB (93.73%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -4,10 +4,18 @@
  * @return {character}
  */
 var nextGreatestLetter = function(letters, target) {
-    for(let word of letters){
-        if(word > target){
-            return word;
+    let left = 0;
+    let right = letters.length - 1;
+    
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        
+        if (letters[mid] <= target) {
+            left = mid + 1; 
+        } else {
+            right = mid - 1; 
         }
     }
-    return letters[0];
+    
+    return letters[left % letters.length];
 };
