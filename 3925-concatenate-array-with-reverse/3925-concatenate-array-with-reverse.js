1ddef3572461cdeae0d1ecc3c752352e557843e2
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-07-02 11:43:29 +0530

    Time: 2 ms (36.62%), Space: 58.1 MB (79.49%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.js
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.js
@@ -3,8 +3,5 @@
  * @return {number[]}
  */
 var concatWithReverse = function(nums) {
-    let arr = []
-    for(let i = nums.length-1; i>=0; i--){
-        arr.push(nums[i]);
-    }return [...nums,...arr]
+    return [...nums,...nums.reverse()]
 };
