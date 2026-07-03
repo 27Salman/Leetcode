074b2e2fdfd7346ffb16ac93f3ecee615d451114
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-07-03 14:59:09 +0530

    Time: 0 ms (100%), Space: 56.3 MB (46.28%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.js
+++ 3945-digit-frequency-score/3945-digit-frequency-score.js
@@ -3,13 +3,5 @@
  * @return {number}
  */
 var digitFrequencyScore = function(n) {
-    let m = n.toString();
-    let freq = {};
-    let sum = 0;
-    for(let i = 0; i<m.length; i++){
-        freq[m[i]] = (freq[m[i]] || 0) + 1;
-    }
-    for(let num in freq){
-        sum += freq[num] * num
-    }return sum
+    return n.toString().split("").reduce((acc,cur) => acc + Number(cur), 0)
 };
