638fb35a8b3c566c6ecafa923474c98661c3d0d4
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-07-11 17:08:02 +0530

    Time: 0 ms (100%), Space: 55.7 MB (50.27%) - LeetHub

--- 3959-check-good-integer/3959-check-good-integer.js
+++ 3959-check-good-integer/3959-check-good-integer.js
@@ -4,7 +4,11 @@
  */
 var checkGoodInteger = function(n) {
     let num = n.toString().split("").map(a=>Number(a));
-    let num1 = num.reduce((acc,cur)=> acc+cur, 0);
-    let num2 = num.reduce((acc,cur)=> acc+cur*cur, 0);
+    let num1 = 0;
+    let num2 = 0;
+    for(let i = 0; i<num.length; i++){
+        num1 += num[i];
+        num2 += num[i]*num[i];
+    }
     return ((num2 - num1) >= 50)
 };
