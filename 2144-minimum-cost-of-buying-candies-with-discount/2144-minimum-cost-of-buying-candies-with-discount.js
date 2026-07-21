54db1e7702fb025f6b9815ae92d6a71f9214b4ca
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-07-21 23:34:10 +0530

    Time: 1 ms (89.16%), Space: 54.6 MB (39.95%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.js
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.js
@@ -5,9 +5,9 @@
 var minimumCost = function(cost) {
     cost.sort((a,b)=> b-a);
     let sum = 0;
-    for(let i = 0; i<cost.length; i++){
-        if(( i + 1) % 3 === 0) continue;
+    for(let i = 0; i<cost.length; i += 3){
         sum += cost[i];
+        if( i + 1 < cost.length) sum += cost[i+1];
     }
     return sum;
 };
