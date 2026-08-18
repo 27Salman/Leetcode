2b4e7189f552b3d390a2159e71b6c633eeaa82d1
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-08-18 21:06:41 +0530

    Time: 3 ms (43.86%), Space: 56.3 MB (96.49%) - LeetHub

---
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var countOppositeParity = function(nums) {
+    let arr = [];
+    for(let i = 0; i<nums.length; i++){
+        let count = 0;
+        if(nums[i]%2 !== 0){
+            for(let j = i+1; j<nums.length; j++){
+                if( nums[j]%2===0 ) count++;
+            }
+            arr.push(count);
+        }else{
+            for(let j = i+1; j<nums.length; j++){
+                if( nums[j]%2 !==0 ) count++;
+            }
+            arr.push(count);
+        }
+    }return arr;
+};
