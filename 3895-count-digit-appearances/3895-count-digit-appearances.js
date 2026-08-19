a10b3b0e8d9c30c1995662b8ef8556b7bdd83fab
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-08-19 10:01:06 +0530

    Time: 20 ms (8.2%), Space: 60.8 MB (29.51%) - LeetHub

--- 3895-count-digit-appearances/3895-count-digit-appearances.js
+++ 3895-count-digit-appearances/3895-count-digit-appearances.js
@@ -4,9 +4,5 @@
  * @return {number}
  */
 var countDigitOccurrences = function(nums, digit) {
-    let count = 0;
-    let occur = nums.join("").split("")
-    for(let i = 0; i<occur.length; i++){
-        if(occur[i]==digit) count++;
-    }return count;
+    return nums.join("").split("").filter( a => a == digit).length;
 };
