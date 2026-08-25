d86d3ce4d59fc27fda7792bd6b2edda8aebc3910
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-08-25 22:32:38 +0530

    Time: 1 ms (48.46%), Space: 55.7 MB (80.55%) - LeetHub

--- 4020-elevator-requests-i/4020-elevator-requests-i.js
+++ 4020-elevator-requests-i/4020-elevator-requests-i.js
@@ -6,12 +6,8 @@
 var elevatorRequests = function(n, requests) {
     let current = 0;
     let totalTime = 0;
-    const len = requests.length;
-    
-    for (let i = 0; i < len; i++) {
-        const req = requests[i];
-        const diff = req - current;
-        totalTime += diff < 0 ? -diff : diff;
-        current = req;
+    for(let req of requests){
+        totalTime += Math.abs(current - req);
+        current = req
     }return totalTime;
 };
