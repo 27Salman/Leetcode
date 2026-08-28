71c65b461d2cd1253e5cac1e01b0bf26acc7cf27
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-08-28 22:48:30 +0530

    Time: 0 ms (100%), Space: 77.3 MB (38.39%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
@@ -3,16 +3,7 @@
  * @return {number}
  */
 var findCenter = function(edges) {
-    let n = edges.length;
-    let nums = edges.flat(Infinity).reduce((acc,cur)=> {
-        acc[cur] = (acc[cur] || 0)+1;
-        return acc;
-    },{});
-    let result = 0
-    for(let num in nums){
-        if(nums[num]===n){
-            result = Number(num);
-        }
-    };
-    return result;
+    return (edges[0][0] === edges[1][0] || edges[0][0] === edges[1][1]) 
+        ? edges[0][0] 
+        : edges[0][1];
 };
