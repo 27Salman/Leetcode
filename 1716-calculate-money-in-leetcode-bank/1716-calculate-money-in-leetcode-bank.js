23bec8652b0bc1fa3ef9c4d599c69c7b246e5292
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2026-09-19 23:32:12 +0530

    Time: 1 ms (38.6%), Space: 54.3 MB (29.82%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.js
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.js
@@ -4,15 +4,15 @@
  */
 var totalMoney = function(n) {
     let total = 0;
-    let mondayMoney = 1;
-    let dailyMoney = 1;
+    let monday = 1;
+    let daily = 1;
 
     for (let day = 1; day <= n; day++) {
-        total += dailyMoney;
-        dailyMoney++;
+        total += daily;
+        daily++;
         if (day % 7 === 0) {
-            mondayMoney++;
-            dailyMoney = mondayMoney;
+            monday++;
+            daily = monday;
         }
     }
 
